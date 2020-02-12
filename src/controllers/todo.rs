7b5c31ad52869cbb661ab33b2f0534dd1f5e8444
explooosion <ta7382@gmail.com>
2020-02-12 20:50:31 +0800

    Add app data state

--- src/controllers/todo.rs
+++ src/controllers/todo.rs
@@ -1,11 +1,18 @@
-use actix_web::{web, Result, HttpResponse, Responder};
+use actix_web::{web, HttpResponse, Responder};
 use serde::{Deserialize, Serialize};
+use std::sync::Mutex;
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Todo {
-  id: i32,
-  name: String,
-  done: bool,
+  pub id: i32,
+  pub name: String,
+  pub done: bool,
+}
+
+pub struct AppState {
+  // pub app_name: String,
+  pub counter: Mutex<i32>,
+  pub todos: Mutex<Vec<Todo>>,
 }
 
 pub async fn get_todo() -> impl Responder {
@@ -16,8 +23,10 @@ pub async fn get_todo() -> impl Responder {
       Todo { id: 3, name: format!("洗澡"), done: false },
     ]
   )
+  // let app_name = &data.app_name; // <- get app_name
+  // HttpResponse::Ok().body(format!("Hello {}!", app_name))
 }
-  
+
 pub async fn add_todo(todo: web::Json<Todo>) -> impl Responder {
   HttpResponse::Ok().json(Todo {
     id: todo.id,
@@ -25,9 +34,11 @@ pub async fn add_todo(todo: web::Json<Todo>) -> impl Responder {
     done: todo.done,
   })
 }
-  
-pub async fn get_todo_by_id(id: web::Path<i32>) -> Result<String> {
-  Ok(format!("Get todo id: {}", id))
+
+pub async fn get_todo_by_id(data: web::Data<AppState>) -> impl Responder {
+  let mut _todos = data.todos.lock().unwrap(); // <- get counter's MutexGuard
+  // let todo = &_todos[0];
+  HttpResponse::Ok().json(&_todos[0])
 }
 
 pub async fn delete_todo_by_id(todo: web::Json<Todo>) -> impl Responder {
@@ -37,7 +48,7 @@ pub async fn delete_todo_by_id(todo: web::Json<Todo>) -> impl Responder {
     done: todo.done,
   })
 }
-  
+
 pub async fn update_todo_by_id(todo: web::Json<Todo>) -> impl Responder {
   HttpResponse::Ok().json(Todo {
     id: todo.id,
--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,48 @@
 mod controllers;
 mod routes;
 
-use actix_web::{middleware::Logger, App, HttpServer};
+use actix_web::{middleware::Logger, web, App, HttpServer};
 use crate::routes::routes;
+use crate::controllers::todo::{AppState, Todo};
+use std::sync::Mutex;
+
+async fn _index(data: web::Data<AppState>) -> String {
+  let mut counter = data.counter.lock().unwrap(); // <- get counter's MutexGuard
+  // *counter += 1; // <- access counter inside MutexGuard
+  *counter += 1;
+  format!("Request number: {}", counter) // <- response with count
+}
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
 
-  let port = 3000;
-
   std::env::set_var("RUST_LOG", "actix_web=info");
   env_logger::init();
 
-  println!("Starting http server: 127.0.0.1:{}", port);
+  let app_state = web::Data::new(AppState {
+   counter: Mutex::new(0),
+   todos: Mutex::new(
+    vec![
+      Todo { id: 1, name: format!("吃飯"), done: false },
+      Todo { id: 2, name: format!("睡覺"), done: true },
+      Todo { id: 3, name: format!("洗澡"), done: false },
+    ]
+   ),
+  });
 
-  HttpServer::new(|| {
+  HttpServer::new(move || {
     App::new()
       .wrap(Logger::default())
+      // .data(AppState {
+      //   app_name: String::from("Actix-web"),
+      // })
+      // .data(AppStateWithCounter {
+      //   counter: Mutex::new(0),
+      // })
+      .app_data(app_state.clone()) // <- register the created data
       .configure(routes)
   })
-  .bind(format!("127.0.0.1:{}", port))?
+  .bind("127.0.0.1:3000")?
   .run()
   .await
 }
